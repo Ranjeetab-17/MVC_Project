1c476880d8f341f5dfb0004d5aab1b434eecac23
ranjeetab17 <ranjeetab17@gmail.com>
2017-12-16 22:55:17 +0530

    Login & Home Controller

---
+++ MVC/MVC/Models/UserLogin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class UserLogin
+    {
+        public string UserID { get; set; }
+        public string Password { get; set; }
+    }
+}
